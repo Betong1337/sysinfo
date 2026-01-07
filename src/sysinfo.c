d2d50061e4ca5b0c6a5ff9509d582886217f41c6
Betong1337 <asp.jakob@outlook.com>
2026-01-07 07:24:46 +0100

    Added version to print_os_type and removed the quotes.

--- src/sysinfo.c
+++ src/sysinfo.c
@@ -8,11 +8,19 @@ void print_title() {
     }
 }
 
+void strip_quotes(char *str) {
+    size_t len = strlen(str);
+
+    if (len >= 2 && str[0] == '"' && str[len - 1] == '"') {
+        str[len - 1] = '\0';          // ta bort sista "
+        memmove(str, str + 1, len);   // ta bort första "
+    }
+}
+
 const char *get_username() {
     struct passwd *pw = getpwuid(getuid());
     return pw ? pw->pw_name : NULL;
 }
-
 
 void print_uptime() {
     FILE *uptime_file = fopen(UPTIME_PATH, "r");
@@ -22,6 +30,7 @@ void print_uptime() {
         while (fgets(line, sizeof(line), uptime_file)) {
             char* uptime_seconds = strtok(line, " ");
             float uptime_hours = (atof(uptime_seconds) / 60) / 60;
+            
             printf("\033[1;34m%s\033[0m", "Uptime: ");
             printf("%.2f hours\n", uptime_hours);
             break;
@@ -45,6 +54,8 @@ void print_os_type() {
     char line[256];
     char *key;
     char *value;
+    char os_type[64];
+    char os_version[64];
 
     if (os_type_file != NULL) {
         while (fgets(line, sizeof(line), os_type_file)) {
@@ -52,8 +63,17 @@ void print_os_type() {
             value = strtok(NULL, "=");  
 
             if (strncmp("NAME", key, 4) == 0) {
+                strcpy(os_type, value);
+            }
+            if (strncmp("VERSION", key, 6) == 0) {
+                strcpy(os_version, value);
+                os_version[strcspn(os_version, "\n")] = '\0';
+                os_type[strcspn(os_type, "\n")] = '\0';
+                strip_quotes(os_version);
+                strip_quotes(os_type);                
                 printf("\033[1;34m%s\033[0m", "OS: ");
-                printf("%s", value);
+                printf("%s %s\n", os_type, os_version);
+                break;
             }
         }
     fclose(os_type_file);
