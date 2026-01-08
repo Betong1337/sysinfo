ee9b7c3b506cb942b3bfea9e36a0557b7b2fe0b8
Betong1337 <asp.jakob@outlook.com>
2026-01-08 22:27:06 +0100

    added color.h

---
+++ include/color.h
@@ -0,0 +1,11 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+//Colors
+#define RED "\033[1;31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+#define HEADER "\033[1;34m"
+
+#endif
