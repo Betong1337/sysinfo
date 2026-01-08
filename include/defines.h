2da9557a248d59c129a31a00eb746922819b3f29
Betong1337 <asp.jakob@outlook.com>
2026-01-08 21:12:54 +0100

    Added defines for main.c

--- include/defines.h
+++ include/defines.h
@@ -1,6 +1,8 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+#define SYSINFO_VERSION "0.1"
+#define SYSINFO_VERSION_MSG "sysinfo v0.1\n"
 //Colors
 #define RED "\033[1;31m"
 #define GREEN "\033[32m"
@@ -38,4 +40,18 @@
 #define OS_MSG "%s%s%s%s %s\n"
 #define OS_NAME_CMP "NAME"
 #define OS_VERSION_CMP "VERSION"
+
+//main.c
+#define CPU_ARGV_CMP "--cpu"
+#define MEM_ARGV_CMP "--mem"
+#define SWAP_ARGV_CMP "--swap"
+#define ALL_ARGV_CMP "--all"
+#define UPTIME_ARGV_CMP1 "--uptime"
+#define UPTIME_ARGV_CMP2 "--up"
+#define SYSINFO_VERSION_ARGV_CMP1 "--version"
+#define SYSINFO_VERSION_ARGV_CMP2 "-v"
+#define OS_ARGV_CMP "--os"
+#define HELP_ARGV_CMP "--help"
+#define KERNEL_VERSION_ARGV_CMP "--kernel-version"
+
 #endif
